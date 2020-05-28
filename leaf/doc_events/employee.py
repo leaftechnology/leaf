8fb2918a308195eb6b88c1538050ec8e6adc7027
jiloysss <jangeles@bai.ph>
2020-05-28 23:55:52 +0800

    Initialization

--- leaf/doc_events/employee.py
+++ leaf/doc_events/employee.py
@@ -12,7 +12,7 @@ def cancel_employee_leave(doc, method):
     if leave_type[0].enable_percentages:
         employee = frappe.db.sql(""" SELECT * FROM `tabEmployee` WHERE name=%s """,(doc.employee),as_dict=1)
         new_leave = employee[0].leave_balance
-        if employee[0].leave_balance > 0:
+        if int(employee[0].leave_balance) > 0:
             new_leave = int(employee[0].leave_balance) - int(doc.total_leaves_allocated) if int(employee[0].leave_balance) - int(doc.total_leaves_allocated) > 0 else 0
         frappe.db.sql(""" UPDATE tabEmployee SET leave_balance=%s WHERE name=%s """,(str(new_leave),doc.employee))
         frappe.db.commit()
